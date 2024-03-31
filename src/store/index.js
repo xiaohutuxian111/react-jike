44ea1301fb9a641c843e46274c6448ca7c9e80bc
石头 <2398391057@qq.com>
2024-03-31 10:53:15 +0800

    使用redux管理token

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './modules/user'
+
+
+
+export default configureStore({
+    reducer: {
+        // 注册子模块
+        user: userReducer
+    }
+})
