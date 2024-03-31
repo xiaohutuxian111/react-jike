58ffa733e22f71df3308adf7decedc42fb599e3a
石头 <2398391057@qq.com>
2024-03-31 10:13:06 +0800

    添加表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -21,29 +21,38 @@ const Login = () => (
             >
                 <p className='title'>欢 迎 登 录</p>
                 <Form.Item
-                    label="用户名"
-                    name="username"
+                    label="手机号"
+                    name="mobile"
                     rules={[
                         {
                             required: true,
-                            message: '请输入用户名',
+                            message: '请输入手机号',
                         },
+                        {
+                            pattern: /^1[3-9]\d{9}$/,
+                            message: "请输入合法手机号"
+                        }
                     ]}
                 >
                     <Input className='username' />
                 </Form.Item>
 
                 <Form.Item
-                    label="密 码"
-                    name="password"
+                    label="验证码"
+                    name="code"
                     rules={[
                         {
                             required: true,
-                            message: '请输入密码',
+                            message: '请输入验证码',
                         },
+                        {
+                            pattern: /\d{6}/,
+                            message: "请输入合法验证码"
+                        }
+
                     ]}
                 >
-                    <Input.Password className='pass' />
+                    <Input.Password className='pass' maxLength={6} />
                 </Form.Item>
                 <div className='check'>
                     <Form.Item
